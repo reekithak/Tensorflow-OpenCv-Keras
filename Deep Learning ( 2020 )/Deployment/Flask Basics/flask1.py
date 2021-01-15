a8f2791d412a27ae57d21a09ccc06e1e5bdfde7b
Akhil Sanker <reekithak@gmail.com>
2021-01-16 02:56:42 +0530

    Next

---
+++ Deep Learning ( 2020 )/Deployment/Flask Basics/flask1.py
@@ -0,0 +1,12 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+@app.route('/',methods=['POST'])
+def add():
+    a = request.form['a']
+    b = request.form['b']
+    return str(int(a)+int(b))
+
+if __name__ =='__main__':
+    app.run(port=7000)
