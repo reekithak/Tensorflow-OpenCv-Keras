64cea47c4fe0c98b467015b367fce5311a0bdd63
Akhil Sanker <reekithak@gmail.com>
2021-06-08 09:01:44 +0530

    add:edits

--- Deep Learning ( 2020 )/Misc/Final_Year_Work(py clg)/Deployment_common/model.py
+++ Deep Learning ( 2020 )/Misc/Final_Year_Work(py clg)/Deployment_common/model.py
@@ -7,12 +7,27 @@ from tensorflow.keras.preprocessing import image
 
 
 
-model = keras.models.load_model("Models\model_only.h5")
-model.load_weights("Models\model_only.hdf5")
-classes = ['glioma_tumor',
-         'meningioma_tumor',
-         'no_tumor',
-         'pituitary_tumor']
+model = keras.models.load_model("Models\model_weights.h5")
+classes = ['diseased cotton leaf',
+         'diseased cotton plant',
+         'fresh cotton leaf',
+         'fresh cotton plant',
+         'Pepper__bell___Bacterial_spot',
+         'Pepper__bell___healthy',
+         'Potato___Early_blight',
+         'Potato___healthy',
+         'Potato___Late_blight',
+         'Tomato___Bacterial_spot',
+         'Tomato___Early_blight',
+         'Tomato___healthy',
+         'Tomato___Late_blight',
+         'Tomato___Leaf_Mold',
+         'Tomato___Septoria_leaf_spot',
+         'Tomato___Spider_mites Two-spotted_spider_mite',
+         'Tomato___Target_Spot',
+         'Tomato___Tomato_mosaic_virus',
+         'Tomato___Tomato_Yellow_Leaf_Curl_Virus'
+         ]
 
 
 def preprocess_image(local_img,target_size=(150, 150)):
